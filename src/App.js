715f9a9c6d5bb8d8a9ca07b5c4b9771c1915afcc
maniaru455 <maniaru455>
2023-07-04 12:00:25 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AddBook from "./Components/Books/AddBook";
+
+import Books from "./Components/Books/Books";
+import { EditBook } from "./Components/Books/EditBook";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import AddStudent from "./Components/Users/AddStudent";
+import AddMentor from "./Components/Users/AddMentor";
+import { EditStudent } from "./Components/Users/EditStudent";
+import Users from "./Components/Users/Users";
+import { EditMentor } from "./Components/Users/EditMentor";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/addbook" element={<AddBook />} />
+        <Route path="editbook/:id" element={<EditBook />} />
+        <Route path="/user" element={<Users />} />
+        <Route path="/addStudent" element={<AddStudent />} />
+        <Route path="/addMentor" element={<AddMentor />} />
+        <Route path="editstudent/:id" element={<EditStudent />} />
+        <Route path="editmentor/:id" element={<EditMentor />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
